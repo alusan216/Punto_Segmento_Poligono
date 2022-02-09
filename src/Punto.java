4fc8618b2686fc2e11a3d4d156f04b2f928c2be5
alusan216 <75945746+alusan216@users.noreply.github.com>
2022-02-09 18:58:37 +0100

    Initial commit

---
+++ src/Punto.java
@@ -0,0 +1,15 @@
+public class Punto {
+    private int x;
+    private int y;
+
+    public Punto() {
+        x = 0;
+        y = 0;
+    }
+    public Punto(int x,int y){
+        this.x= x;
+        this.y= y;
+    }
+
+
+}
